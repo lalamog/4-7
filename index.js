3528a315040cc0af35e04708c111272441f900f6
lalamog <132582290+lalamog@users.noreply.github.com>
2023-05-07 00:14:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const db = require("./routes/db-config");
 const app = express();
 const bodyParser = require("body-parser");
 const cookie = require("cookie-parser");
@@ -17,10 +16,7 @@ app.set("view engine" , "ejs")
 app.set("views" , "./views")
 app.use(cookie())
 app.use(express.json())
-db.connect((err)=>{
-    if(err)throw err;
-    console.log("database connected");
-})
+
 
 app.use("/", require("./routes/pages"));
 app.use("/api", require("./controllers/auth")); 
